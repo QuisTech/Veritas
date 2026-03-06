b83dc1af1eb09f729f1b35bc561a0c8452fae727
QuisTech <michquis@gmail.com>
2026-03-06 15:03:12 +0100

    chore: securely abstract Firebase config to environment variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA9oUgfhjdFhZ2K7AIkD1GAPGuXpIPZA-8",
-  authDomain: "veritas-38225.firebaseapp.com",
-  projectId: "veritas-38225",
-  storageBucket: "veritas-38225.firebasestorage.app",
-  messagingSenderId: "664280908713",
-  appId: "1:664280908713:web:6ee00cf77a3a51faa7bab8",
-  measurementId: "G-GNJC9X2JN9"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
